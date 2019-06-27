5385bf1bc44cbdb08ce33523558cf90f6fc156fa
Florian Erwig <florian@erwig.it>
2019-06-27 21:59:12 +0200

    no message

--- Ext3d/src/ext3d/JFX3d_Perfect.java
+++ Ext3d/src/ext3d/JFX3d_Perfect.java
@@ -7,8 +7,11 @@ import javax.imageio.ImageIO;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.AmbientLight;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.ParallelCamera;
+import javafx.scene.PointLight;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.image.WritableImage;
@@ -25,34 +28,93 @@ public class JFX3d_Perfect extends Application {
 	private static final int SCALE = 5;
 	private static final int WIDTH = 128*SCALE;
 	private static final int HEIGHT = 128*SCALE;
-	private static final int VOXEL_SIZE = 2*SCALE;
+	private static final int VOXEL_SIZE = 3*SCALE;
 
 	public int R = 0;
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 
-		Group model = readImage(new File("basic_chr.png"),32);
 
 
 
 
 		ParallelCamera camera = new ParallelCamera();
-		Group X = new Group();
+		Group lights = new Group();
+		PointLight light = new PointLight();
+		light.setTranslateY(32*VOXEL_SIZE);
+		light.setTranslateZ(-16*VOXEL_SIZE);
+		light.setColor(Color.GRAY);
+		lights.getChildren().addAll(
+			light,
+			new AmbientLight(Color.GRAY)
+		);
+		Group model = new Group();
+		Group mLLeg     = readImage(new File("xmodels/basic_expo-0.png"),32);
+		Group mRLeg     = readImage(new File("xmodels/basic_expo-1.png"),32);
+		Group mRArm = new Group();
+			Group mRLArm    = readImage(new File("xmodels/basic_expo-2.png"),32); mRArm.getChildren().add(mRLArm);
+			Group mRUArm    = readImage(new File("xmodels/basic_expo-3.png"),32); mRArm.getChildren().add(mRUArm);
+		Group mLArm = new Group();
+			Group mLLArm    = readImage(new File("xmodels/basic_expo-4.png"),32); mLArm.getChildren().add(mLLArm);
+			Group mLUArm    = readImage(new File("xmodels/basic_expo-5.png"),32); mLArm.getChildren().add(mLUArm);
+		Group mBody     = readImage(new File("xmodels/basic_expo-6.png"),32);
+			mBody.getChildren().addAll(
+					mRArm,
+					mLArm
+			);
+		model.getChildren().addAll(
+			mLLeg,
+			mRLeg,
+			mBody
+		);
+		{
+			mRArm.setOnMouseClicked(event -> {
+				for(Node n :mRLArm.getChildren()){
+					if(n instanceof Box){
+						if(((Box) n).getDrawMode() == DrawMode.LINE) {
+							((Box) n).setDrawMode(DrawMode.FILL);
+						}else{
+							((Box) n).setDrawMode(DrawMode.LINE);
+						}
+					}
+				}
+			});
+		}
 
+		{
+			mBody.setTranslateY(VOXEL_SIZE);
 
+			mLArm.setTranslateX(1.5*VOXEL_SIZE);
+				mLLArm.setTranslateY(-3*VOXEL_SIZE);
+				mLLArm.setRotationAxis(Rotate.X_AXIS);
+				mLLArm.setRotate(45);
+				mLLArm.setTranslateZ(2.125*VOXEL_SIZE);
+			mLArm.setRotationAxis(Rotate.X_AXIS);
+			mLArm.setTranslateY(-3*VOXEL_SIZE);
+			mLArm.setTranslateZ(2.125*VOXEL_SIZE);
+			mLArm.setRotate(45);
+
+			mRArm.setTranslateX(-1.5*VOXEL_SIZE);
+				mRLArm.setTranslateY(-3*VOXEL_SIZE);
+				mRLArm.setRotationAxis(Rotate.X_AXIS);
+				mRLArm.setRotate(45);
+				mRLArm.setTranslateZ(2.125*VOXEL_SIZE);
+			mRArm.setRotationAxis(Rotate.X_AXIS);
+			mRArm.setTranslateY(-3*VOXEL_SIZE);
+			mRArm.setTranslateZ(2.125*VOXEL_SIZE);
+			mRArm.setRotate(45);
+		}
 
 
 		model.getTransforms().clear();
 		model.getTransforms().addAll(
 				new Rotate(26.565, Rotate.X_AXIS),
-				new Rotate(45, Rotate.Y_AXIS),
-				new Rotate(45, Rotate.Y_AXIS),
-				new Rotate(45, Rotate.Y_AXIS)
+				new Rotate(-90, Rotate.Y_AXIS)
 		);
 		model.setTranslateX(WIDTH/2);
 		model.setTranslateY(HEIGHT/2);
-		X.getChildren().addAll(model);
-		Scene scene = new Scene(X, WIDTH, HEIGHT, true, SceneAntialiasing.BALANCED);
+
+		Scene scene = new Scene(new Group(model,lights), WIDTH, HEIGHT, true, SceneAntialiasing.BALANCED);
 		scene.setFill(Color.TRANSPARENT);
 		scene.setCamera(camera);
 		//model.setRotationAxis(Rotate.Y_AXIS);
@@ -89,8 +151,13 @@ public class JFX3d_Perfect extends Application {
 			BufferedImage bImage = SwingFXUtils.fromFXImage(img, null);
 			ImageIO.write(bImage,"png",new File(dir,"out_"+i+".png"));
 		}
-		//model.setRotationAxis(Rotate.Y_AXIS);
-		//model.setRotate(225);
+		model.getTransforms().clear();
+		model.getTransforms().addAll(
+				new Rotate(26.565, Rotate.X_AXIS),
+				new Rotate(180, Rotate.Y_AXIS)
+		);
+		model.setTranslateX(WIDTH/2);
+		model.setTranslateY(HEIGHT/2);
 		primaryStage.show();
 
 
@@ -109,12 +176,12 @@ public class JFX3d_Perfect extends Application {
 				for(int z = 0; z < img.getWidth(); z++){
 					int px = (img.getRGB(x, z + (y*cellHeight)));
 					if(new java.awt.Color(px, true).getAlpha() > 0) {
-						PhongMaterial material = new PhongMaterial(Color.rgb(
+						Color col = Color.rgb(
 								(px >> 16) & 0xFF,
 								(px >> 8) & 0xFF,
 								(px >> 0) & 0xFF
-						));
-
+						);
+						PhongMaterial material = new PhongMaterial(col);
 						Box c = new Box(VOXEL_SIZE, VOXEL_SIZE, VOXEL_SIZE);
 						c.setCullFace(CullFace.BACK);
 						c.setDrawMode(DrawMode.FILL);
@@ -132,7 +199,7 @@ public class JFX3d_Perfect extends Application {
 		//bottom.setTranslateX(-cellHeight*VOXEL_SIZE/2);
 		//bottom.setTranslateZ(-cellHeight*VOXEL_SIZE/2);
 		bottom.setMaterial(new PhongMaterial(Color.BEIGE));
-		ret.getChildren().add(bottom);
+		//ret.getChildren().add(bottom);
 		Box cell = new Box(cellHeight*VOXEL_SIZE,cellHeight*VOXEL_SIZE,cellHeight*VOXEL_SIZE);
 		cell.setDrawMode(DrawMode.LINE);
 		//cell.setTranslateY(-cellHeight*VOXEL_SIZE/2);
